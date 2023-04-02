f25b4707f6b731593d404c0b87baa5c58c7f9703
vincent <vincent_thejane@outlook.com>
2023-04-02 18:57:51 +0200

    some few tweaks

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,16 @@
 // Path: ./config/env/production/server.js`
 
+// module.exports = ({ env }) => ({
+//   url: env("RENDER_EXTERNAL_URL"),
+//   dirs: {
+//     public: "/data/public"
+//   },
+//   });
+  
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  dirs: {
-    public: "/data/public"
+  proxy: true,
+  url: env('HOST_URL'), // Sets the public URL of the application.
+  app: {
+    keys: env.array('APP_KEYS')
   },
-  });
-  
-  
+});
